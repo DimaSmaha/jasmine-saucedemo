fa463a907abafee1b2968aa07292dea3649f96d7
Dima Smaha <fannifty@gmail.com>
2022-12-28 21:26:27 +0200

    add items

--- test/pageobjects/inventory.page.js
+++ test/pageobjects/inventory.page.js
@@ -10,6 +10,14 @@ class InventoryPage extends Page {
   get backpackItemLink() {
     return $("#item_4_title_link");
   }
+
+  get backpackItemName() {
+    return $('a[id="item_4_title_link"] [class="inventory_item_name"]');
+  }
+
+  get backpackItemDescription() {
+    return $('a[id="item_4_title_link"]~[class="inventory_item_desc"]');
+  }
 }
 
 module.exports = new InventoryPage();
