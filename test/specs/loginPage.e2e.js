e04a8e320a2dae832ed5f6b8e0dea235a480ee4f
Dima Smaha <fannifty@gmail.com>
2022-12-31 09:57:30 +0200

    make all tests runnable

--- test/specs/loginPage.e2e.js
+++ test/specs/loginPage.e2e.js
@@ -3,14 +3,14 @@ const inventoryPage = require("../pageobjects/inventory.page");
 const loginPage = require("../pageobjects/login.page");
 
 describe("Login tests", () => {
-  xit("should login with valid credentials", async () => {
+  it("should login with valid credentials", async () => {
     await LoginPage.open();
     await expect(loginPage.loginCredentials).toHaveClass("login_credentials");
     await LoginPage.doLogin("standard_user", "secret_sauce");
     await expect(inventoryPage.backpackItemLink).toBeExisting();
   });
 
-  xit("should show an error whe use login with invalid credentials", async () => {
+  it("should show an error whe use login with invalid credentials", async () => {
     await LoginPage.open();
     await LoginPage.doLogin("invalid_user", "invalid_password");
     await expect(loginPage.errorLogin).toBeExisting();
