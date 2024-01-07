ec20d3f5bac78790c59af4ad12ebefbd7e48df2a
Dima Smaha <fannifty@gmail.com>
2024-01-07 14:09:12 +0200

    bs

--- wdio.conf.bs.js
+++ wdio.conf.bs.js
@@ -179,7 +179,6 @@ exports.config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter
-  reporters: ["spec"],
 
   //
   // Options to be passed to Jasmine.
@@ -196,7 +195,7 @@ exports.config = {
   },
 
   // ...
-  reporters: [
+  reporters: [ "spec",
     [
       "allure",
       {
