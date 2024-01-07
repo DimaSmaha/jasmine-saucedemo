ff967883320f1fd67fa93a8eadf644111d15dfe9
Dima Smaha <fannifty@gmail.com>
2024-01-07 13:48:15 +0200

    upd

--- wdio.conf.sauce.js
+++ wdio.conf.sauce.js
@@ -58,7 +58,7 @@ exports.config = {
   // and 30 processes will get spawned. The property handles how many capabilities
   // from the same test should run tests.
   //
-  maxInstances: 10,
+  maxInstances: 2,
   //
   // If you have trouble getting all important capabilities together, check out the
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -162,10 +162,6 @@ exports.config = {
       // do something
     },
   },
-
-  // ...
-  reporters: ["spec"],
-  // ...
 
   //
   // =====
